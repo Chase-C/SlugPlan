2a8e008ee04d7696d651f4141268b16ab91d371e
Chase Cummings <chaseecummings@gmail.com>
2016-03-16 03:53:08 -0700

    Courses are now saved

--- Handler/Planner.hs
+++ Handler/Planner.hs
@@ -10,7 +10,7 @@ module Handler.Planner where
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 
-import Import
+import Import hiding (head, last)
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3,
                               withSmallInput)
 import Settings
@@ -21,35 +21,75 @@ import Data.Aeson.Types (Result (..))
 import Database.Persist
 import Database.Persist.Sql
 
---import Data.List (intercalate)
+import Control.Applicative ((<$>))
+import Data.Bits           ((.&.))
+import Data.List           ((!!), head, last)
 
 --------------------------------------------------------------------------------
 
 getPlannerR :: Handler Html
 getPlannerR = do
+    qtrGroups <- groupBy (\(n1, _, _) (n2, _, _) -> n1 == n2) <$> getQtrCourses
+    let qtrCourses      = groupCourses qtrGroups
+        (lastQtr, _, _) = last qtrCourses
     defaultLayout $ do
-        let (courseFormId, courseListId) = courseIds
         setTitle "Course Planner"
         $(widgetFile "planner")
 
+getQtrCourses :: Handler [(Int, Text, (Course, CourseId))]
+getQtrCourses =
+    runDB $ do
+        cs <- selectList [UserCourseUser ==. 1] [Asc UserCourseQuarter]
+        catMaybes <$>
+          mapM (\usrCourse ->
+                   let id  = userCourseCourse  $ entityVal usrCourse
+                       qtr = userCourseQuarter $ entityVal usrCourse
+                   in  do
+                       course <- get id
+                       case course of
+                           (Just c) -> return $ Just (qtr, getQuarter qtr, (c, id))
+                           Nothing  -> return Nothing) cs
+
+groupCourses :: [[(Int, Text, (Course, CourseId))]]
+             -> [(Int, Text, [[(Course, CourseId)]])]
+groupCourses []     = []
+groupCourses (x:xs) =
+    let (num, qtr, _) = head x
+        cs            = map (\(_, _, c) -> c) x
+    in  (num, qtr, groupThree cs) : groupCourses xs
+
+groupThree :: [a] -> [[a]]
+groupThree (a:b:c:xs) = [a,b,c] : groupThree xs
+groupThree []         = []
+groupThree xs         = [xs]
+
+getQuarter :: Int -> Text
+getQuarter n =
+    let year = 2016 + ((n + 1) `div` 4)
+        qtr  = ["Winter", "Spring", "Summer", "Fall"] !! ((n + 1) .&. 3)
+    in  qtr ++ " " ++ tshow year
+
 deleteNewCourseR :: Handler Value
-deleteNewCourseR =
+deleteNewCourseR = do
+    courseId <- (requireJsonBody :: Handler CourseId)
+    runDB $ deleteWhere [UserCourseCourse ==. courseId]
     returnJson ("ok" :: Text)
 
 putNewCourseR :: Handler Value
-putNewCourseR = do
-    courseName <- (requireJsonBody :: Handler Text)
-    mCourse    <- runDB $ selectFirst [CourseName ==. courseName] []
-    -- userCourses <- runDB $ selectList [UserCourseUserName ==. userName] []
-    -- [UserCourse]
-    -- courseId = map userCourseCourse userCourses
+putNewCourseR =
+    returnJson ("ok" :: Text)
+
+postNewCourseR :: Handler Value
+postNewCourseR = do
+    userCourse <- (requireJsonBody :: Handler UserCourse)
+    mCourse    <- runDB $ do
+        c <- get $ userCourseCourse userCourse
+        insert $ UserCourse 1 (userCourseCourse  userCourse)
+                              (userCourseQuarter userCourse)
+        return c
     case mCourse of
         (Just course) -> returnJson course
         _             -> notFound
-
-postNewCourseR :: Handler Value
-postNewCourseR =
-    returnJson ("ok" :: Text)
 
 getNewCourseR :: Handler Value
 getNewCourseR =
